67e8391375aaf8e26d10138ff027c8fa623d9a67
Nimra Tariq <minnayjabeen@gmail.com>
2023-03-13 16:46:34 +0500

    new file

---
+++ if.py
@@ -0,0 +1,14 @@
+#nested if 
+a = 3 
+c = 5
+if a!=b:
+    print('no it is not true')
+    if a < b:
+    print(good work)
+    elif a > b:
+    print('good')
+else
+    print(bad)
+    
+    
+    
